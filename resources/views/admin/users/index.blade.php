eeb8e984d8d5628b7403dbb503bb71739f793f46
GuCMartins <coelhomartins7@gmail.com>
2022-08-19 17:26:50 -0300

    usuarios aparecendo

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,32 +1,25 @@
 @extends('layouts.master')
 
 @section('content')
-        @slot('create', route('users.create'))
-        @slot('titulo', 'Membros')
-        @slot('head')
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th></th>
-        @endslot
-        @slot('body')
-            @foreach ($users as $user)
-                <tr>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td class="options d-flex justify-content-center gap-1">
-                        <a href="{{ route('users.show', $user->id) }}" class="btn btn-dark">
-                            <i class="fas fa-search"></i>
-                        </a>
-
-                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary"><i class="fas fa-pen"></i></a>
-   
-                        <form class="form-delete d-inline-block" action="{{ route('users.destroy', $user->id) }}" method="post">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        @endslot
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th></th>
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td class="options d-flex justify-content-center gap-1">
+                    <a href="{{ route('users.show', $user->id) }}" class="btn btn-dark">
+                        <i class="fas fa-search"></i>
+                    </a>
+                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary"><i class="fas fa-pen"></i></a>
+ 
+                    <form class="form-delete d-inline-block" action="{{ route('users.destroy', $user->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
 @endsection
