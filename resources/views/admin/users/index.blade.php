93df84aa838be2c07c3fc002a9171f1339f7d709
GuCMartins <coelhomartins7@gmail.com>
2022-08-20 19:51:34 -0300

    botoes sendo alinhados/ trabalhar nas factorys

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.master')
 
 @section('content')
-        <th>Nome</th>
-        <th>E-mail</th>
-        <th></th>
+        <div class="row">   
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th></th>
+        </div>    
         @foreach ($users as $user)
             <tr>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td class="options d-flex justify-content-center gap-1">
-                    <a href="{{ route('users.show', $user->id) }}" class="btn btn-dark">
-                        <i class="fas fa-search"></i>
-                    </a>
-                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary"><i class="fas fa-pen"></i></a>
- 
-                    <form class="form-delete d-inline-block" action="{{ route('users.destroy', $user->id) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
-                    </form>
-                </td>
+                <div class="row d-grid gap-2 d-md-block">
+                    <td scope="justify-content-center align-items-center">{{ $user->name }}</td>
+                    <td class="justify-content-start name-itens-table">{{ $user->email }}</td>
+                    <td class="d-flex justify-content-end colum-bottons-table">
+                        <a href="{{ route('users.show', $user->id) }}" class="btn btn-dark" style=" height:4vh ; width:4vw">
+                                <i class="fas fa-search"></i>
+                        </a>
+                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary" style="height:4vh ; width:4vh"><i class="fas fa-pen"></i></a>
+        
+                        <form class="form-delete d-flex" action="{{ route('users.destroy', $user->id) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-danger" style="height:4vh ; width:4vh"><i class="fas fa-trash"></i></button>
+                        </form> 
+                    </td>
+                </div>    
             </tr>
         @endforeach
 @endsection
