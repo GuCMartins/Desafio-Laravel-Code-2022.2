ea52a24dc642c51eafd6f534370f6e10512e41d4
GuCMartins <coelhomartins7@gmail.com>
2022-09-01 14:54:10 -0300

    usuario mostrando os dados corretamente

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -4,8 +4,12 @@
     <div class="show">
         <a href="{{ route('users.index') }}" type="botton" class="btn btn-secondary">Voltar</a>
         <p>Nome : {{ $user->name }}</p>
-        <p>Sabor : {{ $user->email }}</p>
-        <p>Preço : {{ $user->association }}</p>
-        <p>Descrição : {{ $user->permission }}</p>
-    </div>        
+        <p>Email : {{ $user->email }}</p>
+        <p>Associação : {{ $user->association }}</p>
+        @if ($user->permission == true)
+            <p>Permissão : Admnistrador</p>
+        @else
+            <p>Permissão : Cliente</p>
+        @endif
+    </div>
 @endsection
